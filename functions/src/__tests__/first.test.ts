1a7328aa57afdfecfff03ec7f658100378b7ff00
Lebit <spam@labitte.com>
2021-05-24 20:32:42 +0200

    first work on testing fb cloud function

---
+++ functions/src/__tests__/first.test.ts
@@ -0,0 +1,44 @@
+import * as fbtest from "firebase-functions-test";
+import * as admin from "firebase-admin";
+
+import * as myFunctions from "../index";
+
+beforeEach(() => {
+  fbtest().cleanup();
+  fbtest().firestore.clearFirestoreData({ projectId: "embit-dev" });
+});
+
+test("Cannot create invite if not an admin", () => {
+  try {
+    fbtest().wrap(myFunctions.generateInvite);
+  } catch (error) {
+    expect(error.message).toBe("Not authorized");
+  }
+});
+
+test("Can create invite if an admin", () => {
+  const wrapped = fbtest().wrap(myFunctions.generateInvite);
+  wrapped(
+    {},
+    {
+      auth: {
+        token: {
+          role: "admin",
+        },
+      },
+    }
+  );
+  admin
+    .firestore()
+    .collection("invites")
+    .get()
+    .then((invites) => {
+      expect(invites.size).toBe(1);
+    });
+});
+
+//TODO register admin account
+//TODO login with admin account
+//TODO generate Invite
+//TODO register regular account
+//TODO login with regular account
